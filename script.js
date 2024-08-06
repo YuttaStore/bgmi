1dabd92fb6297923cb5c89db6c484349c49c20da
YuttaStore <metubing21@gmail.com>
2024-08-06 16:55:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const paymentMethod = document.querySelector('input[name="payment-method"]:checked');
         const paymentOption = paymentMethod ? paymentMethod.value : 'Not selected';
         const timestamp = new Date().toLocaleString();
-        const productId = selectedPackage ? selectedPackage.querySelector('.details h3').textContent + " UC" : 'Not selected'; // Get the selected package ID
+        const productId = selectedPackage ? selectedPackage.querySelector('.details h3').textContent + " UC(BGMI)" : 'Not selected'; // Get the selected package ID
 
         // Construct the invoice message
         const invoiceMessage = `
